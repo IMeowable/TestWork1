82c863eda9b8a35f1a53aaa55bfe88af4b140513
IMeowable <151074908+IMeowable@users.noreply.github.com>
2023-12-18 21:25:52 +0300

    Update TestWork.h

--- TestWork.h
+++ TestWork.h
@@ -1,13 +1,7 @@
-﻿// TestWork.h : включаемый файл для стандартных системных включаемых файлов
-// или включаемые файлы для конкретного проекта.
-
 #pragma once
 using namespace std;
 #include <iostream>
-
-
-#include <iostream> 
 #include <vector> 
 #include <algorithm> 
 
-// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+
